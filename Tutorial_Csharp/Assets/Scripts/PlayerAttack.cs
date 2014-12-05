df3ac0fbd1a7f0265f18397077a5df0dc433b906
Yaching Chiu <ychiu@kabam.com>
2014-12-04 17:09:52 -0800

    First commit for this project

---
+++ Tutorial_Csharp/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.F)) {
+			Attack();                                                
+		}
+	}
+
+	private void Attack() {
+		float distance = Vector3.Distance(target.transform.position, transform.position);
+		Debug.Log("Distance = " + distance);
+
+		if(distance < 2.5 ) {
+			EnemyHealth eh = (EnemyHealth)target.GetComponent("EnemyHealth");
+			eh.AdjuestCurtHealth(-10);
+		}
+	}
+}
