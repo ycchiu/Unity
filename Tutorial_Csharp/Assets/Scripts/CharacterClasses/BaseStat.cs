340c5784184267eca8c222ee2005eed26384a489
yachingc <tracy720329@gmail.com>
2014-12-08 07:14:07 -0800

    progress - video 15 fin

---
+++ Tutorial_Csharp/Assets/Scripts/CharacterClasses/BaseStat.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseStat : MonoBehaviour {	
+	private int _baseValue;			// the base value of this stat
+	private int _buffValue;			// the amount fo th buff to this stat
+	private int _expToLevel;		// the total amount of exp needed to raise this skill
+	private float _levelModifier;	// the modifier applied tothe exp needed to rails this skill
+
+	public BaseStat () {
+		_baseValue = 0;
+		_buffValue = 0;
+		_levelModifier = 1.1f;
+		_expToLevel = 100;
+	}
+
+
+	public int BaseValue {
+		get{ return _baseValue; }
+		set{ _baseValue = value; }
+	}
+
+	public int BuffValue {
+		get{ return _buffValue; }
+		set{ _buffValue = value; }
+	}
+
+	public int ExpToLevel {
+		get{ return _expToLevel; }
+		set{ _expToLevel = value; }
+	}
+
+	public float LevelModifier {
+		get{ return _levelModifier; }
+		set{ _levelModifier = value; }
+	}
+
+
+	private int CalculateExpToLevel() {
+		return (int)(_expToLevel * _levelModifier);
+	}
+
+	public void LevelUp() {
+		_expToLevel = CalculateExpToLevel ();
+		_baseValue++;
+	}
+
+	public int AdjustedValue() {
+		return _baseValue + _buffValue;
+	}
+}
---
+++ Tutorial_Csharp/Assets/Scripts/CharacterClasses/ModifiedStat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+public class ModifiedStat : BaseStat {
+	private List<ModifyingAttribute> _mods;		//A listof Attributes that modify this stat
+	private int _modValue;						//The amount added to the baseValue from the modifiers
+
+	public ModifiedStat() {
+		_mods = new List<ModifyingAttribute> ();
+		_modValue = 0;
+	}
+
+	public void AddModifier(ModifyingAttribute mod) {
+		_mods.Add (mod);
+	}
+
+	private void CalculateModValue() {
+		_modValue = 0;
+
+		if (_mods.Count > 0) {
+			foreach(ModifyingAttribute att in _mods) 
+				_modValue += (int)(att.attribute.AdjustedValue * att.ratio);
+		}
+	}
+}
+
+public struct ModifyingAttribute {
+	public Attribute attribute;
+	public float ratio;
+}
