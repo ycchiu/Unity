7777e5caaad73d166fcc1c077a86b06941b9a0e8
yachingc <tracy720329@gmail.com>
2014-12-07 19:05:03 -0800

    Finish tutorial 1-10

--- Tutorial_Csharp/Assets/Scripts/Targetting.cs
+++ Tutorial_Csharp/Assets/Scripts/Targetting.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 
 public class Targetting : MonoBehaviour {
+	private Transform myTransform;
+
 	public List<Transform> targets;
 	public Transform selectedTarget;
+
+	// Anything happened before script
+	void Awake() {
+		myTransform = this.transform;
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -15,11 +22,10 @@ public class Targetting : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(KeyCode.Tab)) {
+		if(Input.GetKeyUp(KeyCode.Tab)) {
 			TargetEnemy();
 		}
 	}
-
 
 	public void AddAllEnemies() {
 		GameObject[] go = GameObject.FindGameObjectsWithTag("Enemy");
@@ -35,6 +41,48 @@ public class Targetting : MonoBehaviour {
 	}
 
 	private void TargetEnemy() {
-		selectedTarget = targets[0];
+		if (selectedTarget == null) 
+		{
+			SortTargetByDistance ();
+			selectedTarget = targets [0];
+		} 
+		else 
+		{
+			int index = targets.IndexOf(selectedTarget);
+			if(index < targets.Count-1) {
+				index += 1;
+			}
+			else {
+				index = 0;
+			}
+
+			UnselectTarget();
+			selectedTarget = targets [index];
+		}
+
+		if (selectedTarget != null) {
+			SelectTarget();
+		}
 	}
+
+	private void SelectTarget() {
+		selectedTarget.renderer.material.color = Color.red;
+
+		PlayerAttack pa = (PlayerAttack) GetComponent("PlayerAttack");
+
+		pa.target = selectedTarget.gameObject;
+	}
+
+	private void UnselectTarget() {
+		selectedTarget.renderer.material.color = Color.white;
+		selectedTarget = null;
+	}
+
+	private void SortTargetByDistance() {
+		targets.Sort ( delegate (Transform t1, Transform t2){
+			return Vector3.Distance(t1.position, myTransform.position).CompareTo(Vector3.Distance(t2.position
+			                                                                                      , myTransform.position));		
+		});
+	}
+
 }
