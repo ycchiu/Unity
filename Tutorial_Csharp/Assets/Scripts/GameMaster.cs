d25f36356e452eca5aa4a4b2a19bb9c91c1fb7c7
yachingc <tracy720329@gmail.com>
2014-12-09 20:28:01 -0800

    video 34 in progress

--- Tutorial_Csharp/Assets/Scripts/GameMaster.cs
+++ Tutorial_Csharp/Assets/Scripts/GameMaster.cs
@@ -3,11 +3,27 @@ using System.Collections;
 
 public class GameMaster : MonoBehaviour {
 	public GameObject playerCharacter;
+	public Camera mainCamera;
 
+	public float zOffset;
+	public float yOffset;
+	public float xRotation;
+
+	private GameObject _pcCached;
 
 	// Use this for initialization
 	void Start () {
-		Instanitiate(playerCharacter, Vector3.zero, Quaternion.identity);
+		_pcCached = Instantiate(playerCharacter, Vector3.zero, Quaternion.identity) as GameObject;
+
+		zOffset = -2.5f;
+		yOffset = 2.0f;
+		xRotation = 20.0f;
+
+		mainCamera.transform.position = new Vector3 (_pcCached.transform.position.x, 
+		                                             _pcCached.transform.position.y + yOffset, 
+		                                             _pcCached.transform.position.z + zOffset);
+		mainCamera.transform.Rotate (xRotation, 0, 0);
+
 	}
 	
 	// Update is called once per frame
