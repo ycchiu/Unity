cbbc8b54e29b3d37ef41cd31b8bb2e747aa002b8
Yaching Chiu <ychiu@kabam.com>
2014-12-09 17:53:32 -0800

    Progress video 33

---
+++ Tutorial_Csharp/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+	public GameObject playerCharacter;
+
+
+	// Use this for initialization
+	void Start () {
+		Instanitiate(playerCharacter, Vector3.zero, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
